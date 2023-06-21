981898840552056aaa7a1e46502da2bf03d78510
Ellee007 <alhassanaliyu83@gmail.com>
2023-06-21 04:40:40 -0700

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,13 +15,11 @@ int main(void)
 {
 		int i, sum = 0;
 
-			for (i = 0; i < 1024; i++)
-					{
-								if ((i % 3) == 0 || (i % 5) == 0)
+		for (i = 0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
 												sum += i;
-									}
-
-				printf("%d\n", sum);
-
-					return (0);
+		}
+		printf("%d\n", sum);
+		return (0);
 }
