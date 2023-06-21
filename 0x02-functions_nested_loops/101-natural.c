653f2fa254eb1a9024bc75b0b3669844b6f0797e
Ellee007 <alhassanaliyu83@gmail.com>
2023-06-21 05:37:23 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 /*
- *  * File: 101-natural.c
- *   * Auth: Aliyu
+ * File: 101-natural.c
+ * Auth: Aliyu Alhassan
  */
 
 #include <stdio.h>
@@ -13,13 +13,15 @@
  */
 int main(void)
 {
-		int i, sum = 0;
+	int i, sum = 0;
 
-		for (i = 0; i < 1024; i++)
-		{
-			 if ((i % 3) == 0 || (i % 5) == 0)
-			         sum += i;
-		}
-		printf("%d\n", sum);
-		return (0);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
