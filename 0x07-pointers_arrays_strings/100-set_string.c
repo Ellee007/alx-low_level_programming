2cca69833d166edbcfe9ea7cc69d7ba6fa5251b5
Ellee007 <alhassanaliyu83@gmail.com>
2023-07-03 06:50:05 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,13 +2,9 @@
 
 
 /**
-
  * set_string - sets the value of a pointer to a char
-
  * @s: pointer to pointer
-
  * @to: pointer char
-
  */
 
 
@@ -16,6 +12,6 @@ void set_string(char **s, char *to)
 
 {
 
-        *s = to;
+	*s = to;
 
 }
