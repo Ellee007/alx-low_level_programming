ea45b5be46cd0e6e091718a6f13cc4eae8f29d24
Ellee007 <alhassanaliyu83@gmail.com>
2024-03-11 09:39:34 -0700

    algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t lb, size_t ub);
+#endif
